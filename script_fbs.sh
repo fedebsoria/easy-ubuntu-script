17275d65d50b644c459b551314d99afaaddf7484
Federico B. Soria <fsoria7@outlook.com>
2025-11-10 14:35:03 +0100

    Start making the script for SO subject from the FP ASIR

---
+++ script_fbs.sh
@@ -0,0 +1,33 @@
+# CRIPT
+# LINUX
+# Crear un menú con 30 opciones:
+# 1. Muestra la versión de Linux
+# 2. Crear archivos con nano
+# 3. Crear archivos con gedit
+# 4. Instalar herramientas de red (samba)
+# 5. Instalar herramientas de grupos
+# 6. Visualizar carpetas y archivos
+# 7. Asignar permisos
+# 8. Visualiza calendario
+# 9. Visualizar vaquita
+# simpática 10.Manual de un
+# comando
+# 11.Mostrar archivos sin posibilidad de editarlo
+# 12.Información de un archivo especifico
+# 13.Vincular archivos
+# 14.Reiniciar terminal
+# 15.Crear carpetas
+# 16.Crear archivos
+# 17.Crear usuarios
+# 18.Crear grupos
+# 19.Copiar carpetas
+# 20.Eliminar carpeta
+# 21.Eliminar archivo
+# 22.Agregar un usuario a un grupo
+# 23.Listar usuarios
+# 24.Listar grupos
+# 25.Actualizar el sistema
+# 26.Salir
+# NOTA
+# Continuar hasta 30 con opciones inventadas por vosotros
+
